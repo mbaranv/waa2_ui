a07ca2442395dd3d74c5a007eb8aa222442d5012
mbaranv <gfbarann@gmail.com>
2023-10-22 18:00:01 +0300

    test

--- src/test/java/runner/Test.java
+++ src/test/java/runner/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Olur inşallah");
         System.out.println("Olur inşallah");
+        System.out.println("Olur inşallah");
     }
 }
