df3f41a40073340494fce3d397a022d58bc89a7d
mbaranv <gfbarann@gmail.com>
2023-10-22 17:57:07 +0300

    test

--- src/test/java/runner/Test.java
+++ src/test/java/runner/Test.java
@@ -3,5 +3,6 @@ package runner;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Olur inşallah");
+        System.out.println("Olur inşallah");
     }
 }
